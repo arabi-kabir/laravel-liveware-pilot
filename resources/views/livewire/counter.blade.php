e60af3ab89a2641aa6081b479bedb40a7fafab97
Arabi Kabir <mail2arabi@gmail.com>
2021-06-23 01:34:25 +0600

    laravel-liveware-2 done

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div style="text-align: center">
+    <button style="font-size: 150%" wire:click="increment">+</button>
+
+    <h2>{{ $count }}</h2>
+
+    <button style="font-size: 150%" wire:click="decrement">-</button>
+</div>
